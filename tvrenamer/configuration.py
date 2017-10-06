c4eb86e3275cf89285c8786821545fc124d227ee
Daniel Collins <jugglindan@gmail.com>
2017-10-06 12:53:38 +0800

    removed config files and logging

--- tvrenamer/configuration.py
+++ tvrenamer/configuration.py
@@ -2,109 +2,15 @@
 """ Configuration and command-line arguments
 """
 
-import os
-import shutil
+import argparse
 
-import configargparse
-from pkg_resources import Requirement, resource_filename
-
-
-def get_config_file(basename):
-    """ Looks for a configuration file in 3 locations:
-
-        - the current directory
-        - the user config directory (~/.config/tvrenamer)
-        - the version installed with the package (using setuptools resource API)
-
-    Args:
-        basename (str): The base filename.
-
-    Returns:
-        str: The full path to the configuration file.
+def get_configuration():
+    """Parses and returns the program configuration options.
     """
-    locations = [
-        os.path.join(os.curdir, basename),
-        os.path.join(
-            os.path.expanduser("~"),
-            ".config",
-            "tvrenamer",
-            basename),
-        resource_filename(
-            Requirement.parse("tvrenamer"),
-            os.path.join('tvrenamer', basename))
-    ]
+    parser = argparse.ArgumentParser(
+        add_help=True)
 
-    for location in locations:
-        if os.path.isfile(location):
-            return location
-
-def copy_default_config_to_user_directory(
-        basename,
-        clobber=False,
-        dst_dir='~/.config/tvrenamer'):
-    """ Copies the default configuration file into the user config directory.
-
-    Args:
-        basename (str): The base filename.
-        clobber (bool): If True, the default will be written even if a user
-            config already exists.
-        dst_dir (str): The destination directory.
-    """
-    dst_dir = os.path.expanduser(dst_dir)
-    dst = os.path.join(dst_dir, basename)
-    src = resource_filename(
-        Requirement.parse("tvrenamer"),
-        os.path.join('tvrenamer', basename))
-
-    if not os.path.exists(dst_dir):
-        os.makedirs(dst_dir)
-
-    if clobber or not os.path.isfile(dst):
-        shutil.copy(src, dst)
-
-def get_configuration(basename='tvrenamer.cfg', parents=None):
-    """Parses and returns the program configuration options,
-    taken from a combination of ini-style config file, and
-    command line arguments.
-
-    Args:
-        basename (str): The base filename.
-        parents (list): A list of ArgumentParser objects whose arguments
-            should also be included in the configuration parsing. These
-            ArgumentParser instances **must** be instantiated with the
-            `add_help` argument set to `False`, otherwise the main
-            ArgumentParser instance will raise an exception due to duplicate
-            help arguments.
-
-    Returns:
-        The options object.
-    """
-    copy_default_config_to_user_directory(basename)
-
-    parser = configargparse.ArgParser(
-        formatter_class=configargparse.ArgumentDefaultsRawHelpFormatter,
-        add_help=True,
-        parents=parents or [],
-        default_config_files=[
-            resource_filename(
-                Requirement.parse("tvrenamer"),
-                os.path.join('tvrenamer', basename)),
-            os.path.join(
-                os.path.expanduser("~/.config/tvrenamer"),
-                basename),
-            os.path.join(os.curdir, basename)])
-
-    # logging config file
-    parser.add(
-        '-lc',
-        '--logging-config',
-        required=False,
-        default='tvrenamer_logging.cfg',
-        metavar='FILE',
-        env_var='TVRENAMER_LOGGING_CONFIG',
-        help='Logging configuration file')
-
-    parser.add(
+    parser.add_argument(
         '-t',
         '--threshold',
         required=False,
@@ -113,40 +19,38 @@ def get_configuration(basename='tvrenamer.cfg', parents=None):
         help='Similarity threshold for show name fuzzy matching',
         )
 
-    parser.add(
+    parser.add_argument(
         '-v',
         '--version',
-        required=False,
-        action='store_true',
-        help='''Display tvrenamer version''')
+        action='version',                    
+        version='%(prog)s (version 0.3.0)')
 
-    parser.add(
+    parser.add_argument(
         '-vb',
         '--verbose',
         required=False,
         action='store_true',
         help='''Verbose reporting mode''')
 
-    parser.add(
+    parser.add_argument(
         '-dr',
         '--dry-run',
         required=False,
         action='store_true',
         help='''Conduct a dry run. No changes are written to file''')
 
-    parser.add(
+    parser.add_argument(
         '-p',
         '--path',
         required=True,
         metavar='DIRECTORY',
         help='''The directory to scan for files''')
 
-    parser.add(
+    parser.add_argument(
         '-id',
         '--showid',
         required=True,
         type=str,
         help='''TVDB show id for matching episodes''')
-
 
     return parser.parse_known_args()[0]
--- tvrenamer/tvrenamer.py
+++ tvrenamer/tvrenamer.py
@@ -2,96 +2,65 @@
 """ tvdb-renamer: TV show file renamer
 """
 
-import logging
-import logging.config
 import os
 from glob import glob
 
-from .configuration import (
-    get_configuration,
-    get_config_file,
-    copy_default_config_to_user_directory)
+from .configuration import get_configuration
 from.episode_matcher import get_best_match
 from .metadata import __author__, __email__, __version__
 from .tvdb_scraper import read_show_html, parse_show_data
-
-def __init_logging(logging_config_file):
-    """
-    Initialises logging.
-
-    Args:
-        logging_config_file (str): The logging configuration file.
-        """
-
-    # Make sure the user copy of the logging config file exists
-    copy_default_config_to_user_directory(logging_config_file, clobber=False)
-
-    # Load the config
-    logging.config.fileConfig(get_config_file(logging_config_file))
 
 def start_cli():
     """ Runs the renamer."""
 
     config = get_configuration()
-    __init_logging(config.logging_config)
-    logging.getLogger(__name__).info('renamer version %s', __version__)
-    logging.getLogger(__name__).info('verbose mode: %s', config.verbose)
-
-    if config.version:
-        return
+    print('version {0}'.format(__version__))
 
     if config.dry_run:
-        print('**** Dry run, no files changed')
+        print('**** Dry run, no files changed ****')
 
-    try:
-        # get show data
-        title, html = read_show_html(config.showid)
-        data = parse_show_data(html)
-        print('**** {0} ****'.format(title))
-        if config.verbose:
-            print(data)
+    # get show data
+    title, html = read_show_html(config.showid)
+    data = parse_show_data(html)
+    print('**** {0} ****'.format(title))
+    if config.verbose:
+        print(data)
 
-        # Parse the files
-        print('Scanning for files at {0}'.format(config.path))
-        search_path = os.path.join(config.path, '*')
-        for f in glob(search_path):
-            base, ext = os.path.splitext(f)
-            basename = os.path.basename(base)
-            directory = os.path.dirname(f)
+    # Parse the files
+    print('Scanning for files at {0}'.format(config.path))
+    search_path = os.path.join(config.path, '*')
+    for f in glob(search_path):
+        base, ext = os.path.splitext(f)
+        basename = os.path.basename(base)
+        directory = os.path.dirname(f)
 
-            best_match, best_ratio = get_best_match(data, basename)
-            if best_match:
-                new_name = 's{season:02d}-e{episode:03d}-{title}-{name}'.format(
-                    season=best_match['season'],
-                    episode=best_match['episode'],
-                    title=title,
-                    name=best_match['name'])
+        best_match, best_ratio = get_best_match(data, basename)
+        if best_match:
+            new_name = 's{season:02d}-e{episode:03d}-{title}-{name}'.format(
+                season=best_match['season'],
+                episode=best_match['episode'],
+                title=title,
+                name=best_match['name'])
 
-                if basename == new_name:
-                    if config.verbose:
-                        print('    Already named correctly')
-                    continue
+            if basename == new_name:
+                if config.verbose:
+                    print('    Already named correctly')
+                continue
 
-                print('\nChecking {0}'.format(basename))
+            print('\nChecking {0}'.format(basename))
 
-                if best_ratio >= config.threshold:
-                    print('    Similarity score: {0}'.format(best_ratio))
-                    print('    --> {0}'.format(new_name))
-                    if not config.dry_run:
-                        new_name = os.path.join(directory, new_name) + ext
-                        os.rename(f, new_name)
-                else:
-                    print('    Below threshold')
-                    print('    Similarity score: {0}'.format(best_ratio))
-                    print('    Best Match: {0}'.format(new_name))
+            if best_ratio >= config.threshold:
+                print('    Similarity score: {0}'.format(best_ratio))
+                print('    --> {0}'.format(new_name))
+                if not config.dry_run:
+                    new_name = os.path.join(directory, new_name) + ext
+                    os.rename(f, new_name)
             else:
-                print('    no match')
-
-    except Exception as exception:
-        logging.getLogger(__name__).error(exception, exc_info=True)
-
-    logging.getLogger(__name__).info("Exiting")
-
+                print('    Below threshold')
+                print('    Similarity score: {0}'.format(best_ratio))
+                print('    Best Match: {0}'.format(new_name))
+        else:
+            print('    no match')
 
 if __name__ == 'main':
     start_cli()
